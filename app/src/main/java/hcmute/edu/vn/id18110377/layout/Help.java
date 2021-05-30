35bd4f4c48527aa06267bc5df10fe7e1bfb12495
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-05-30 22:41:27 +0700

    add Help feature

--- app/src/main/java/hcmute/edu/vn/id18110377/layout/Help.java
+++ app/src/main/java/hcmute/edu/vn/id18110377/layout/Help.java
@@ -1,6 +1,9 @@
 package hcmute.edu.vn.id18110377.layout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,16 @@ public class Help extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.help);
+        setContentView(R.layout.feature_help);
+
+        ImageButton ibtn = findViewById(R.id.btnBack_help);
+        ibtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        WebView webView = findViewById(R.id.webHelp);
+        webView.loadUrl("https://github.com/tiennhm");
     }
 }
