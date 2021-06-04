886be09ba1a566d9ab3ad2dad2dca8403e71523f
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-06-04 17:45:04 +0700

    code refactor in HomeFragment.java

--- app/src/main/java/hcmute/edu/vn/id18110377/fragment/HomeFragment.java
+++ app/src/main/java/hcmute/edu/vn/id18110377/fragment/HomeFragment.java
@@ -83,15 +83,28 @@ public class HomeFragment extends Fragment {
 
         ProductDbHelper productDbHelper = new ProductDbHelper(this.getContext());
         //Promos
+        setPromoItem(view, productDbHelper);
+
+        //Products
+        setProductItem(view, productDbHelper);
+
+        return view;
+    }
+
+    private void setPromoItem(View view, ProductDbHelper productDbHelper) {
         List<Product> promoProducts = productDbHelper.getPromoProducts(4);
         ProductAdapter productAdapter = new ProductAdapter(getContext(), promoProducts);
         GridView gv_promo = view.findViewById(R.id.homePromo);
+        gv_promo.setOnItemClickListener((parent, view1, position, id) -> {
+            Intent intent = new Intent(this.getContext(), ProductDetail.class);
+            startActivity(intent);
+        });
         gv_promo.setAdapter(productAdapter);
+    }
 
-        //Products
+    private void setProductItem(View view, ProductDbHelper productDbHelper) {
         ProductTypeDbHelper productTypeDbHelper = new ProductTypeDbHelper(this.getContext());
         List<ProductType> productTypes = productTypeDbHelper.getAllProductTypes();
-
         ProductTypeAdapter productTypeAdapter = new ProductTypeAdapter(getContext(), productTypes);
         GridView gv_product = view.findViewById(R.id.homeProduct);
         gv_product.setOnItemClickListener((parent, view1, position, id) -> {
@@ -99,7 +112,5 @@ public class HomeFragment extends Fragment {
             startActivity(intent);
         });
         gv_product.setAdapter(productTypeAdapter);
-
-        return view;
     }
 }
