dd4fe6cbbbb6443a15d839579dfae5460cb43e01
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-06-12 11:49:49 +0700

    remove TODO

--- app/src/main/java/hcmute/edu/vn/id18110377/fragment/MenuFragment.java
+++ app/src/main/java/hcmute/edu/vn/id18110377/fragment/MenuFragment.java
@@ -18,18 +18,11 @@ import hcmute.edu.vn.id18110377.R;
 import hcmute.edu.vn.id18110377.adapter.RecyleItemViewAdapter;
 import hcmute.edu.vn.id18110377.entity.MenuItem;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MenuFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MenuFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -37,15 +30,6 @@ public class MenuFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MenuFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MenuFragment newInstance(String param1, String param2) {
         MenuFragment fragment = new MenuFragment();
         Bundle args = new Bundle();
