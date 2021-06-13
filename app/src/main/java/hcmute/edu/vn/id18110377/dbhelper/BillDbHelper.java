7d11dd0b97ea42a8da2bb8715ada6c031186ba4a
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2021-06-13 21:28:15 +0700

    Update Table Bill

--- app/src/main/java/hcmute/edu/vn/id18110377/dbhelper/BillDbHelper.java
+++ app/src/main/java/hcmute/edu/vn/id18110377/dbhelper/BillDbHelper.java
@@ -15,25 +15,23 @@ import hcmute.edu.vn.id18110377.entity.Bill;
 public class BillDbHelper extends SQLiteOpenHelper {
     private static final String TABLE_NAME = "Bill";
     private static final String BILL_ID = "id";
-    private static final String BILL_USERID = "userId";
-    private static final String BILL_CARTID = "cartId";
-    private static final String BILL_TOTAL = "total";
+    private static final String BILL_USER_ID = "userId";
+    private static final String BILL_CART_ID = "cartId";
     private static final String BILL_DATE = "date";
     private static final String BILL_STATUS = "status";
 
     public BillDbHelper(@Nullable Context context) {
-        super(context, TABLE_NAME, null, DbHelper.DATABASE_VERSION);
+        super(context, DbHelper.DATABASE_NAME, null, DbHelper.DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         String query =
-                "CREATE TABLE Bill ( " +
+                "CREATE TABLE IF NOT EXISTS Bill ( " +
                         "    id    INTEGER NOT NULL, " +
                         "    userId    INTEGER NOT NULL, " +
                         "    cartId    INTEGER NOT NULL, " +
-                        "    total    REAL NOT NULL, " +
-                        "    date    TEXT NOT NULL, " +
+                        "    date    TEXT, " +
                         "    status    TEXT, " +
                         "    FOREIGN KEY(cartId) REFERENCES Cart(id), " +
                         "    FOREIGN KEY(userId) REFERENCES User(id), " +
@@ -47,33 +45,20 @@ public class BillDbHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
     }
 
-    public ArrayList<Bill> getAllBills(Integer userID) {
-        ArrayList<Bill> bills = new ArrayList<>();
-        SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE userId = ?", new String[]{userID.toString()});
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            bills.add(
-                    new Bill(
-                            cursor.getInt(0),
-                            cursor.getInt(1),
-                            cursor.getInt(2),
-                            cursor.getDouble(3),
-                            cursor.getString(4),
-                            cursor.getString(5)
-                    )
-            );
-            cursor.moveToNext();
-        }
-        cursor.close();
-        return bills;
+    private Bill cursorToBill(Cursor cursor) {
+        return new Bill(
+                cursor.getInt(0),
+                cursor.getInt(1),
+                cursor.getInt(2),
+                cursor.getString(3),
+                cursor.getString(4)
+        );
     }
 
     private ContentValues createContentValues(Bill bill) {
         ContentValues values = new ContentValues();
-        values.put(BILL_USERID, bill.getUserId());
-        values.put(BILL_CARTID, bill.getCartId());
-        values.put(BILL_TOTAL, bill.getTotal());
+        values.put(BILL_USER_ID, bill.getUserId());
+        values.put(BILL_CART_ID, bill.getCartId());
         values.put(BILL_DATE, bill.getDate());
         values.put(BILL_STATUS, bill.getStatus());
         return values;
@@ -88,11 +73,44 @@ public class BillDbHelper extends SQLiteOpenHelper {
     public int update(Bill bill) {
         SQLiteDatabase db = getWritableDatabase();
         ContentValues values = createContentValues(bill);
-        return db.update(TABLE_NAME, values, BILL_ID + " = ?", new String[]{String.valueOf(bill.getId())});
+        return db.update(
+                TABLE_NAME, values, BILL_ID + " = ?",
+                new String[]{String.valueOf(bill.getId())});
     }
 
     public int delete(Bill bill) {
         SQLiteDatabase db = getWritableDatabase();
-        return db.delete(TABLE_NAME, BILL_ID + " = ?", new String[]{String.valueOf(bill.getId())});
+        return db.delete(TABLE_NAME, BILL_ID + " = ?",
+                new String[]{String.valueOf(bill.getId())});
+    }
+
+    public ArrayList<Bill> getAllBills(Integer userId) {
+        ArrayList<Bill> bills = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(
+                "SELECT * FROM " + TABLE_NAME + " WHERE " + BILL_USER_ID + " = ?",
+                new String[]{String.valueOf(userId)});
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            bills.add(cursorToBill(cursor));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return bills;
+    }
+
+    public ArrayList<Bill> getUnpaidBills(Integer userId) {
+        ArrayList<Bill> bills = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery(
+                "SELECT * FROM " + TABLE_NAME + " WHERE " + BILL_USER_ID + " = ? AND " + BILL_STATUS + " = " + Bill.BILL_UNPAID,
+                new String[]{String.valueOf(userId)});
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            bills.add(cursorToBill(cursor));
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return bills;
     }
 }
--- app/src/main/java/hcmute/edu/vn/id18110377/entity/Bill.java
+++ app/src/main/java/hcmute/edu/vn/id18110377/entity/Bill.java
@@ -1,28 +1,31 @@
 package hcmute.edu.vn.id18110377.entity;
 
+import hcmute.edu.vn.id18110377.utilities.AppUtilities;
+
 public class Bill {
+    public static final String BILL_UNPAID = "Unpaid";
+    public static final String BILL_PAID = "Paid";
     private Integer id;
     private Integer userId;
     private Integer cartId;
-    private Double total;
     private String date;
     private String status;
+    private Cart cart;
 
-    public Bill(Integer id, Integer userId, Integer cartId, Double total, String date, String status) {
+    public Bill(Integer id, Integer userId, Integer cartId, String date, String status) {
         this.id = id;
         this.userId = userId;
         this.cartId = cartId;
-        this.total = total;
         this.date = date;
         this.status = status;
     }
 
-    public Bill(Integer id, Integer userId, Integer cartId, Double total) {
-        this(id, userId, cartId, total, null, null);
+    public Bill(Integer id, Integer userId, Integer cartId) {
+        this(id, userId, cartId, AppUtilities.getDateTimeNow(), BILL_UNPAID);
     }
 
-    public Bill(Integer userId, Integer cartId, Double total) {
-        this(-1, userId, cartId, total, null, null);
+    public Bill(Integer userId, Integer cartId) {
+        this(-1, userId, cartId, AppUtilities.getDateTimeNow(), BILL_UNPAID);
     }
 
     public Integer getId() {
@@ -49,15 +52,6 @@ public class Bill {
         this.cartId = cartId;
     }
 
-    public Double getTotal() {
-        return total;
-    }
-
-    public void setTotal(Double total) {
-        if (total >= 0)
-            this.total = total;
-    }
-
     public String getDate() {
         return date;
     }
@@ -72,5 +66,13 @@ public class Bill {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
     }
 }
